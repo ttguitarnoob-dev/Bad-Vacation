0fe5a4bd874160c3a71944aa7729551713da39fe
ttguitarnoob <ttguitarnoob@gmail.com>
2022-01-04 19:51:57 -0600

    added win and lose events, various failed styling experiments. MVP is essentially complete!

--- game.js
+++ game.js
@@ -1,16 +1,16 @@
 
 
 const scenes = [
-      {
+    startingScene0 = {
         text: 'You wake up in your bedroom in the middle of the night.',
         options: ['Walk to the bathroom', 'Pick up your phone', 'Pet the kitty'],
 
     },
-     {
+    bathroomScene1 = {
         text: "You enter the bathroom and discover a wonderful smell",
         options: ['Ignore the smell and sit on the toilet', 'Walk to the kitchen'],
     },
-     {
+    kitchenScene2 = {
         text: "You enter the kitchen and discover that you have literally nothing for breakfast.  How does this happen every day?",
         options: ['Eat a head of lettuce', 'drink water from the faucet']
     }
@@ -19,7 +19,12 @@ const scenes = [
 const optionsButtons = document.getElementById("choice-btns")
 const sceneText = document.getElementById("text")
 let currentScene = scenes[0]
+const restart = document.getElementById('restart-button')
 
+restart.addEventListener('click', function(){
+    setScene(0)
+    window.location.reload()
+})
 function startGame(){
     // currentScene = scenes[0]
     setScene(0)
@@ -64,7 +69,7 @@ function choiceMade(e){
             }
             if (currentScene === scenes[1]) {
                 if (boxClicked === 'btnid0'){
-                    console.log('plop plop splash sphffff')
+                    gameOver('you pooped so hard that your liver came out and you died')
                 } else if (boxClicked === 'btnid1'){
                     setScene(2)
                 } else if (boxClicked === 'btnid2'){
@@ -72,14 +77,31 @@ function choiceMade(e){
                 }
                 return
             }
-            
+            if (currentScene === scenes[2]) {
+                if (boxClicked === 'btnid0'){
+                    gameOver('That lettuce is so rotted that you died')
+                } else if (boxClicked === 'btnid1'){
+                    gameWin('Your house just happened to have magic water being pumped into it at this moment.  You become a superhero and join the Avengers')
+                } else if (boxClicked === 'btnid2'){
+                    console.log('purrpurrpurrCHOMP')
+                }
+                return
+            }
 
 
             //end of the line
 
 }
 
-
-
+function gameOver(message){
+    box = document.getElementById('game-over')
+    box.style.display = "flex"
+    box.innerText = message
+}
+function gameWin(message){
+    box = document.getElementById('game-win')
+    box.style.display = "flex"
+    box.innerText = message
+}
 
 startGame()
