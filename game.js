fee98894b6525b15290909eb123e2d58be7dfdc2
ttguitarnoob <ttguitarnoob@gmail.com>
2022-01-06 16:08:25 -0600

    wrote several more story scenes and got them all linked together.

--- game.js
+++ game.js
@@ -20,12 +20,12 @@ let scenes = [
         options: ['Talk to her', 'Walk back down the hall the other way']
     },
     talkWoman4 = {
-        text: 'Kiara:\n    "You have got to help me! I...I woke up to a crash outside my door, and noticed that my daughter was not in her bed.  I have been looking for an hour and there is no sign of her anywhere! I have no idea what else to do...I mean there is only so much space inside this hotel right?  I hope she stayed inside the hotel, because something seems off out there. Please will you let me know if you see her?  She is 6 years old and wearing a Pokemon shirt."',
+        text: 'KIARA:\n    "You have got to help me! I...I woke up to a crash outside my door, and noticed that my daughter was not in her bed.  I have been looking for an hour and there is no sign of her anywhere! I have no idea what else to do...I mean there is only so much space inside this hotel right?  I hope she stayed inside the hotel, because something seems off out there. Please will you let me know if you see her?  She is 6 years old and wearing a Pokemon shirt."',
         options: ['Turn and walk down the hall the other way', '"Yes, I will keep an eye out!"']//if you choose yes, she'll give you a stuffed pokemon, which will populate an option for the scene where you find the child
     },
     stupid5 = {
-        text: "As you turn to walk away from the woman, you see someone walk inside a door down the hallway.  You hurry to catch up with them and get to the door, which is still cracked.",
-        options: ['Take a deep breath', 'Continue']
+        text: 'The childs eyes go wide and she lets out an excited shriek when she sees the Stuffed Pokemon.\nHAZEL:\n"Charmy!! My mom gave that to you didnt she? I wanted to go back, but I lost the door to the stairs and the man by the elevator told me not to use it, but I didnt want to stay in the room with those goo monsters looking in the window."\n\nHer eyes get scared again, and she grabs the Stuffed Pokemon from you. You extend your hand and she takes it.  You take her back down to the 2nd floor, but her mom is no longer in the hallway.  Hazel runs over to room 208 and knocks. Kiara opens the door and bursts into happy tears. She thanks you profusely and they retreat into the room. That felt good! You remember that you have yet to explore the rooms on the 3rd floor.',
+        options: ['Head back to the 3rd floor']
     },
     leftVoices6 = {
         text: "You walk down the hall towards the voices that you heard.  They get louder as you get further down the hall, and eventually you see one of the rooms has a cracked door.  The voices are definitely coming from in there. ",
@@ -36,9 +36,37 @@ let scenes = [
         options: ['First Floor', 'Third Floor'],
     },
     thirdFloor8 = {
+        text: "You take the elevator to the third floor and look down the hallway when the doors open.  You see a sign with directions on the wall.\n<---Pool/Hot Tub/Sauna\nrooms 301-310--->",
+        options: ['Go left', 'Go right']
+    },
+    leftPool9 = {
+        text: "You turn left and walk towards the Swimming Pool area.  There is a single door leading into a large room containing a pool and hot tub.  You enter the room and look around.  There is an old man sitting on one of the beach chairs reading a book. On the other side of him is a set of doors for Men's and Women's locker rooms, and at the end of that same wall is a door that leads to a Sauna.",
+        options: ["Look in the Men's locker room", "Look in the Women's locker room", "Talk to the old man", "Look in the Sauna"],//only sauna will be a new scene.  Hazel is inside. 
+    },
+    sauna10 = {
+        text: "You walk over to the Sauna door and push it open. You look around and at first it seems to be empty.  You are about to leave when you hear a soft squeak from behind the door.  You look around the door and see a little girl hiding behind it looking up at you in fear.",
+        options: ['Try to talk to her', 'Offer her your hand', 'Go back into the pool room'], //this will populate with an option to give her the pokemon toy if you accepted the offer from Kiara
+    },
+    poolAgain11 = {
+        text: "You go back into the pool room. What do you do before checking the rooms on the other side of the hallway?",
+        options: ["Look in the Men's locker room", "Look in the Women's locker room", "Talk to the man", "Check the 3rd floor rooms"],
+    },
+    roomsRight12 = {
+        text: "You head over to the side of the hall containing all of the rooms.  It's peculiar because most of the doors are wide open, and as you walk by them it doesn't seem that anyone is in them.  Rooms 305 and 309 are the only ones closed.",
+        options: ['Yell into each room', 'Knock on 305', 'Knock on 309'], //only 309 has new scene.  yell causes dr to peek out of 309 and then close the door again.  knock on 305 is just nobody answered.
+    },
+    findDoctor13 = {
+        text: 'You knock on room 309. After about 10 seconds the door opens a crack, held in place by the chain. You can see a wide eye peeking out from the inside.\nDR.HANOVER:\n"What do you want? Did Matthew send you? Oh that does not matter what matters is the anomalous situation outside, have you looked out there? My bet is that it is very unwise to go outside, at least without proper precautions, which is why I was hoping someone would come up here. Thing is, I was here for a biology conference and just happen to have some test equipment, and I would love to take a look at that black substance that the people outside have secreted. A vacationing diver brought me his oxygen tank and regulator, but problem is, my head is too small for the damn thing and it leaks right above my nose.  Hey, you want to take a shot at it?',
+        options: ['"Not a good idea..."', '"Sure, what do you need me to do?"'], //only sure makes new scene. no makes dr mad and give you no choice but to agree
+    },
+    gooMissionAccept14 = {
+        text: 'DR. HANOVER\n"Ok thank you...so really all I need is a small sample of that unknown substance.  I will give you a sealed container to put it in, and make sure it seals completely because last thing we need is to expose that to the air in this hotel. Should be pretty simple, just take the elevator to the first floor and find the nearest specimen and take a sample.  Here, let me get you fitted up."\nHe hands you a small sample container and fits the diving gear to your back and face.  It fits perfectly and the fresh oxygen revitalizes you. Dr. Hanover pats your back and gives you a reassuring nod.',
+        options: ['Continue to Elevator'],
+    },
+    firstFloor15 = {
         text: "",
-        options: []
-    }
+        options: [],
+    },
     //end of array
 ]
 
@@ -57,7 +85,7 @@ function startGame(){
 
 function setScene(scene){
     currentScene = scenes[scene]
-    console.log(currentScene)
+    // console.log(currentScene)
     sceneText.innerText = currentScene.text
     while (optionsButtons.firstChild){
         optionsButtons.removeChild(optionsButtons.firstChild)
@@ -141,13 +169,18 @@ function choiceMade(e){
                             return
                         }else {inventory.push('Stuffed Pokemon')
                         addItem('Stuffed Pokemon')
-                        sceneText.innerText = 'Kiara:\n"Thank you so much!  Here take this.  She will trust you if you give her this."\n\nShe hands you a stuffed Pokemon character and you think "Wow this girl must love Pokemon."'
+                        sceneText.innerText = 'KIARA:\n"Thank you so much!  Here take this.  She will trust you if you give her this."\n\nShe hands you a stuffed Pokemon character and you think "Wow this girl must love Pokemon."'
                         }
+                    }
+                    if (scenes[10].options.includes('Show her the Stuffed Pokemon')){
+                        return
+                    }else if(inventory.includes('Stuffed Pokemon')){
+                        scenes[10].options.push('Show her the Stuffed Pokemon')
                     }
                     return
                 }
                 //walk away from woman
-            if (currentScene === scenes[5]) {
+            if (currentScene === scenes[100]) {
                 if (boxClicked === 'btnid0'){
                     return
                 }else if (boxClicked === 'btnid1'){
@@ -165,10 +198,10 @@ function choiceMade(e){
                     addItem('Steel Pipe')
                     
                     }
-                    sceneText.innerText = 'You reach to push open the door, but right as you do, it swings open revealing a very tired looking man in his mid 40s. You ask him if he knows what is going on and he shakes his head slowly side to side.\nMATTHEW:\n"We have yet to reach a solid conclusion, but what we do know is that it aint safe to go outside. I did hear that someone knows of a Doctor staying on the 3rd floor. We were about to send someone up there...hey would you mind going up there and seeing if you can find him? Here take this, we also heard that some of the people are gettin a little crazy. Oh! And make sure you do NOT take the elevator to the 1st floor.  The front door got breached and the air aint breathable."\n He hands you a snapped steel pipe and goes back inside the room.'
+                    sceneText.innerText = 'You reach to push open the door, but right as you do, it swings open revealing a very tired looking man in his mid 40s. You ask him if he knows what is going on and he shakes his head slowly side to side.\nMATTHEW:\n"We have yet to reach a solid conclusion, but what we do know is that it aint safe to go outside. I did hear that someone knows of a Doctor staying on the 3rd floor. I think it was room 309. We were about to send someone up there...hey would you mind going up there and seeing if you can find him? Here take this, we also heard that some of the people are gettin a little crazy. Oh! And make sure you do NOT take the elevator to the 1st floor.  The front door got breached and the air aint breathable."\n He hands you a snapped steel pipe and goes back inside the room.'
                 }
                 return
-        } 
+        } //elevator
             if (currentScene === scenes[7]) {
                 if (boxClicked === 'btnid0'){
                     gameOver("You ride the elevator down to the first floor.  When the doors open, all of the air gets sucked out of the elevator. Slowly, it becomes difficult to breath and then suddenly black ooze bursts out of your lungs and you collapse in a heap.  GAME OVER")
@@ -177,6 +210,88 @@ function choiceMade(e){
                 }
                 return
         }
+        if (currentScene === scenes[8]) {
+            if (boxClicked === 'btnid0'){
+                setScene(9)
+            } else if (boxClicked === 'btnid1'){
+                setScene(12)
+            }
+            return
+        }
+        if (currentScene === scenes[9]) {
+                if (boxClicked === 'btnid0'){
+                    sceneText.innerText = "You enter the Men's locker room and look around, but there is nobody in there so you go back out to the main pool room."
+                } else if (boxClicked === 'btnid1'){
+                    sceneText.innerText = "You open the door to the Women's locker room, and there are two women doing their hair in the mirror.  They look at you shocked and you awkwardly shut the door and run away without saying anything."
+                } else if (boxClicked === 'btnid2'){
+                    sceneText.innerText = 'You walk over to the old man and he looks up from his book\nOLD MAN:\n"Hey there, kid, you come for a swim or just lookin around? Me? I took the chance to come sit in the hot tub while their aint no kids around, though I did see one run into the sauna an hour or so ago.  I dont do too well with all the noise they cause you understand. Seem awful quiet though, even considerin aint nobody around. Might wanna head to the coffee shop down the street later.  Well I gotta finish this chapter, you have a great mornin young man."\nyou take a quick second to warn him not to go outside and he nods in appreciation. He looks back at his book and you turn to walk away.'
+                } else if (boxClicked === 'btnid3'){
+                    setScene(10)
+                } 
+                return
+             }
+             if (currentScene === scenes[10]) {
+                    if (boxClicked === 'btnid0'){
+                        sceneText.innerText = "When you speak, her eyes get wider and she shrinks further into the corner."
+                    } else if (boxClicked === 'btnid1'){
+                        sceneText.innerText = "You extend your hand and she gives a pitiful yelp and slaps it away."
+                    } else if (boxClicked === 'btnid2'){
+                        setScene(11)
+                    } else if (boxClicked === 'btnid3'){
+                        removeItem('Stuffed Pokemon')
+                        setScene(5)
+                    } 
+                    return
+                 }
+                 if (currentScene === scenes[5]) {
+                    if (boxClicked === 'btnid0'){
+                        setScene(12)
+                    } else if (boxClicked === 'btnid1'){
+                        setScene(12)
+                    }
+                    return
+                }
+                if (currentScene === scenes[11]) {
+                    if (boxClicked === 'btnid0'){
+                        sceneText.innerText = "You enter the Men's locker room and look around, but there is nobody in there so you go back out to the main pool room."
+                    } else if (boxClicked === 'btnid1'){
+                        sceneText.innerText = "You open the door to the Women's locker room, and there are two women doing their hair in the mirror.  They look at you shocked and you awkwardly shut the door and run away without saying anything."
+                    } else if (boxClicked === 'btnid2'){
+                        sceneText.innerText = 'You walk over to the old man and he looks up from his book\nOLD MAN:\n"Hey there, kid, you come for a swim or just lookin around? Me? I took the chance to come sit in the hot tub while their aint no kids around, though I did see one run into the sauna an hour or so ago.  I dont do too well with all the noise they cause you understand. Seem awful quiet though, even considerin aint nobody around. Might wanna head to the coffee shop down the street later.  Well I gotta finish this chapter, you have a great mornin young man."\nyou take a quick second to warn him not to go outside and he nods in appreciation. He looks back at his book and you turn to walk away.'
+                    } else if (boxClicked === 'btnid3'){
+                        setScene(12)
+                    } 
+                    return
+                 }
+                 if (currentScene === scenes[12]) {
+                        if (boxClicked === 'btnid0'){
+                            sceneText.innerText = "You walk down the hall yelling into each room to see if anyone answers. After you yell into room 307, the door to 309 opens and an angry looking head pokes out and yells at you to keep it down. He shuts the door with frustration."
+                        } else if (boxClicked === 'btnid1'){
+                            sceneText.innerText = "You knock on the door to room 305 and wait for about 30 seconds.  It seems nobody is in there."
+                        } else if (boxClicked === 'btnid2'){
+                            setScene(13)
+                        }
+                        return
+                     }
+                     if (currentScene === scenes[13]) {
+                        if (boxClicked === 'btnid0'){
+                            sceneText.innerText = 'DR. HANOVER:\n"Look here, bud. You think I wouldnt go right now if this regulator fit my face? Grow some balls man, I know you got a sense that the situation out there is dangerous, so why not try to help someone? You gonna do this?"'
+                        } else if (boxClicked === 'btnid1'){
+                            if (inventory.includes('Sample Container')){
+                                return
+                            }else {inventory.push("Sample Container")
+                            addItem('Sample Container')
+                            }
+                            setScene(14)
+                        }
+                        return
+                    }
+                    if (currentScene === scenes[14]) {
+                        if (boxClicked === 'btnid0'){
+                            setScene(15)
+                        }
+                        return
+                    }
             //end of the line
 
 }
@@ -236,6 +351,7 @@ startGame()
 //     } else if (boxClicked === 'btnid3'){
         
 //     } 
+//     return
 //  }
 //add option to scene on item pickup
 // if (inventory.includes('Phone')){
